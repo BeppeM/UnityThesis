50d4c74f6e4e8d97eb81ff297d59e811316ca479
g.mirra <g.mirra@campus.unimib.it>
2025-02-14 16:05:31 +0100

    Fix agent sight

--- Assets/script/avatar/ConeCollider.cs
+++ Assets/script/avatar/ConeCollider.cs
@@ -31,7 +31,7 @@ public class ConeCollider : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         GameObject obj = other.gameObject;
-        if (obj.layer == LayerMask.NameToLayer("artifact"))
+0        if (obj.layer == LayerMask.NameToLayer("artifact"))
         {
             Debug.Log("Agent " + root.name + " has seen the artifact " + other.name);            
             mainAvatarScript.SetBaloonText("Artifact seen: " + other.name.FirstCharacterToLower());
